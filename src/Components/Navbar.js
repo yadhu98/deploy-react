5ad3dac012a54367191819dd07c58de8a22d1a3d
Yadhu <yadhu98@gmail.noreply.github.com>
2021-05-28 02:44:34 +0530

    responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,8 +9,8 @@ import MessageIcon from '@material-ui/icons/Message';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import GridOnIcon from '@material-ui/icons/GridOn';
 import AddBoxIcon from '@material-ui/icons/AddBox';
-import Slider from 'react-slide-out';
 import 'react-slide-out/lib/index.css';
+import Avatar from '@material-ui/core/Avatar';
 export default function Navbar(handleMouseDown) {
     const [isOpen, setIsopen] = useState(false)
     const togglePopup =()=>{
@@ -19,6 +19,12 @@ export default function Navbar(handleMouseDown) {
     return (
        
         <div className="navbar">
+            <div className="navbaronlyformobile"><Avatar className="avatarmobile"src="https://cdn-images-1.medium.com/max/1200/1*NpUUls7kjn9JhO4ChjGV7w.png"/>
+            <div className="searchboxformobile"><SearchIcon/><input className="searchbarmobile" type="text"/></div>
+               
+            <MessageIcon style={{color : 'Gray'}}/>
+
+            </div>
             <div className="navbar-left">
                 <img src='https://brandeps.com/icon-download/L/Linkedin-icon-vector-13.svg' 
                 alt =""
@@ -29,17 +35,19 @@ export default function Navbar(handleMouseDown) {
                 <input type="text" className="search-box"/>
             </div>   
             <div className="navbar-right">
+                <div className="stickybottom">
                 <div className="navbar-right-icons">
                 <div className="home-container"><NavbarMaterials Icon={HomeIcon} title={"Home"}/></div>
                 <div className="network-container"><NavbarMaterials Icon={SupervisorAccountIcon} title={"My Network"}/></div>
-                <div className="Jobs-container"><NavbarMaterials Icon={BusinessCenterIcon} title={"Jobs"}/></div>
                 <div className="AddPost-container"><NavbarMaterials Icon={AddBoxIcon} title={"Post"}/></div>
+                <div className="Jobs-container"><NavbarMaterials Icon={BusinessCenterIcon} title={"Jobs"}/></div>
                 <div className="Messaging-container"><NavbarMaterials Icon={MessageIcon} title={"Messaging"}/></div>
                 <div className="notifications-container"><NavbarMaterials Icon={NotificationsIcon} title={"Notifications"}/></div>
                 </div>
+                </div>
                 {/* <NavbarMaterials Icon={MessageIcon} title={"Messaging"}/>}
                 <NavbarMaterials Icon={GridOnIcon} title={"Work"}/> */}
-                <div className="avatar-container"><NavbarMaterials avatar="https://cdn-images-1.medium.com/max/1200/1*NpUUls7kjn9JhO4ChjGV7w.png" title="me"/></div>
+             <div className="avatarcontainedincontainer"> <div className="avatar-container"><NavbarMaterials avatar="https://cdn-images-1.medium.com/max/1200/1*NpUUls7kjn9JhO4ChjGV7w.png" title="me"/></div></div>
                 <div className = "work-container"><NavbarMaterials onClick={togglePopup} Icon={GridOnIcon} title={"Work"}/></div>
             
             </div>
